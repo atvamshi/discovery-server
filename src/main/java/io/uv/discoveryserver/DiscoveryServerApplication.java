72899947c516661e557148405e09f0168fb829fc
vamshi <atvamshi@gmail.com>
2019-07-21 21:50:18 -0500

    First commit

---
+++ src/main/java/io/uv/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,18 @@
+package io.uv.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+//		new SpringApplicationBuilder(DiscoveryServerApplication.class).web(true).run(args);
+//		new SpringApplicationBuilder(DiscoveryServerApplication.class).run(args);
+	}
+
+}
